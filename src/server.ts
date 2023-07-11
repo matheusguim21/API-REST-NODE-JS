3d4137e5f7551260108cc443a34eb9e08815542a
matheusguim21 <matheusguim13@gmail.com>
2023-07-11 16:11:03 -0300

    Usando zod e dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,27 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from'node:crypto' 
+import { env } from '../env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
 
-  return tables
+  const transaction = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title:'transação de Teste',
+    amount: 1000,
+  }).returning('*')
+  
+  //const transactions = await knex('transactions').select('*')
+
+  return transaction
+
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333')
