063c021c1a2ac06193d2caf499015942a913c431
matheusguim21 <matheusguim13@gmail.com>
2023-08-14 17:08:57 -0300

    Production Config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import { app } from "./app";
 import { env } from "./env";
 
-app
-  .listen({
-    port: env.PORT,
-  })
-  .then(() => {
-    console.log("Servidor rodando!");
-  });
+const port = env.PORT;
+const host = "RENDER" in process.env ? "0.0.0.0" : "localhost";
+
+app.listen({ host, port }).then(() => {
+  console.log("Servidor rodando!");
+});
